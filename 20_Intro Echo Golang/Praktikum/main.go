90aae71bff9a26e5d17b28563476a96e0ae895cc
wussh <99675213+wussh@users.noreply.github.com>
2022-03-30 19:18:25 +0700

    Add files via upload

---
+++ 20_Intro Echo Golang/Praktikum/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+type User struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var users []User
+
+// -------------------- controller --------------------
+
+// get all users
+func GetUsersController(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success get all users",
+		"users":    users,
+	})
+}
+
+// get user by id
+func GetUserController(c echo.Context) error {
+	ID := c.Param("id")
+	int1, _ := strconv.ParseInt(ID, 6, 12)
+	finduser := users[int1-1]
+	return c.JSON(http.StatusOK, finduser)
+}
+
+// // delete user by id
+// func DeleteUserController(c echo.Context) error {
+// 	// your solution here
+// }
+
+// // update user by id
+// func UpdateUserController(c echo.Context) error {
+// 	// your solution here
+// }
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	// binding data
+	user := User{}
+	c.Bind(&user)
+
+	if len(users) == 0 {
+		user.Id = 1
+	} else {
+		newId := users[len(users)-1].Id + 1
+		user.Id = newId
+	}
+	users = append(users, user)
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"messages": "success create user",
+		"user":     user,
+	})
+}
+
+// ---------------------------------------------------
+func main() {
+	e := echo.New()
+	// routing with query parameter
+	e.GET("/users", GetUsersController)
+	e.GET("/users/:id", GetUserController)
+	e.POST("/users", CreateUserController)
+
+	// start the server, and log if it fails
+	e.Logger.Fatal(e.Start(":8000"))
+}
