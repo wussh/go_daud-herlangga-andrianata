ce4aaad80eb25deff856a136300ce5c7a781cda4
wussh <99675213+wussh@users.noreply.github.com>
2022-04-04 14:13:55 +0700

    Add files via upload

--- 21_ORM and Code Structure (MVC)/Praktikum/main.go
+++ 21_ORM and Code Structure (MVC)/Praktikum/main.go
@@ -1,118 +1,116 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var (
-	DB *gorm.DB
-)
-
-func init() {
-	InitDB()
-	InitialMigration()
-}
-
-type Config struct {
-	DB_Username string
-	DB_Password string
-	DB_Port     string
-	DB_Host     string
-	DB_Name     string
-}
-
-func InitDB() {
-
-	config := Config{
-		DB_Username: "root",
-		DB_Password: "root123",
-		DB_Port:     "3306",
-		DB_Host:     "localhost",
-		DB_Name:     "crud_go",
-	}
-
-	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		config.DB_Username,
-		config.DB_Password,
-		config.DB_Host,
-		config.DB_Port,
-		config.DB_Name,
-	)
-
-	var err error
-	DB, err = gorm.Open("mysql", connectionString)
-	if err != nil {
-		panic(err)
-	}
-}
+var DB *gorm.DB
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name" form:"name"`
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
+	Name  string `json:"name" form:"name"`
+	Email string `json:"email" form:"email"`
 }
 
-func InitialMigration() {
+func initDB() {
+	dsn := "root:minato123@tcp(localhost:3306)/testingorm?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	initMigrate()
+}
+
+func initMigrate() {
 	DB.AutoMigrate(&User{})
 }
 
-// get all users
-func GetUsersController(c echo.Context) error {
+func GetUser(c echo.Context) error {
 	var users []User
-
 	if err := DB.Find(&users).Error; err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		return c.String(http.StatusInternalServerError, "Internal Server Error")
 	}
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "success get all users",
-		"users":   users,
-	})
+	return c.JSON(http.StatusOK, users)
 }
 
-// get user by id
-func GetUserController(c echo.Context) error {
-	// your solution here
-}
-
-// create new user
-func CreateUserController(c echo.Context) error {
+func CreateUser(c echo.Context) error {
 	user := User{}
-	c.Bind(&user)
-
-	if err := DB.Save(&user).Error; err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	if err := c.Bind(&user); err != nil {
+		return c.String(http.StatusInternalServerError, "Internal Server Error")
 	}
-	return c.JSON(http.StatusOK, map[string]interface{}{
-		"message": "success create new user",
-		"user":    user,
-	})
+	if err := DB.Save(&user).Error; err != nil {
+		return c.String(http.StatusInternalServerError, "Internal Sever Error")
+	}
+	return c.JSON(http.StatusOK, user)
 }
 
-// delete user by id
-func DeleteUserController(c echo.Context) error {
-	// your solution here
+func GetUsers(c echo.Context) error {
+	userId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.String(http.StatusBadRequest, "invalid id")
+	}
+	var users User
+	if err := DB.First(&users, userId).Error; err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	if users.ID == 0 {
+		return c.String(http.StatusNotFound, "user not found")
+	}
+	return c.JSON(http.StatusOK, users)
 }
 
-// update user by id
-func UpdateUserController(c echo.Context) error {
-	// your solution here
+func UpdateUser(c echo.Context) error {
+	userId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.String(http.StatusBadRequest, "invalid id")
+	}
+	var users User
+	if err := DB.First(&users, userId).Error; err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	if users.ID == 0 {
+		return c.String(http.StatusNotFound, "user notfound")
+	}
+	if err := c.Bind(&users); err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	if err := DB.Save(&users); err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	return c.JSON(http.StatusOK, users)
+}
+
+func DeleteUser(c echo.Context) error {
+	userId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	var users User
+	if err := DB.First(&users, userId).Error; err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	if users.ID == 0 {
+		return c.String(http.StatusNotFound, "user not found")
+	}
+	if err := DB.Delete(&users, userId).Error; err != nil {
+		return c.String(http.StatusInternalServerError, "internal server error")
+	}
+	return c.JSON(http.StatusOK, users)
 }
 
 func main() {
-	// create a new echo instance
+	initDB()
 	e := echo.New()
-	// Route / to handler function
-	e.GET("/users", GetUsersController)
-	e.GET("/users/:id", GetUserController)
-	e.POST("/users", CreateUserController)
-	e.DELETE("/users/:id", DeleteUserController)
-	e.PUT("/users/:id", UpdateUserController)
-
-	// start the server, and log if it fails
-	e.Logger.Fatal(e.Start(":8000"))
+	e.POST("/users/", CreateUser)
+	e.GET("/users/", GetUser)
+	e.GET("/users/:id", GetUsers)
+	e.PUT("/users/:id", UpdateUser)
+	e.DELETE("/users/:id", DeleteUser)
+	e.Start(":8080")
 }
